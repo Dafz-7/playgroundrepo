afb041214266a9417efcdbec9a660b62405aa429
Dafz-7 <orangksp@gmail.com>
2024-06-29 21:18:56 +0700

    C++ test

---
+++ C++/Simple_battleship_game.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //! Determine the position
+    int ship_placement[4][5] = {
+        {0, 1, 0, 0, 1}, 
+        {0, 1, 0, 0, 1}, 
+        {1, 1, 1, 1, 0}, 
+        {0, 1, 0, 1, 0}
+    };
+
+
+
+
+}
