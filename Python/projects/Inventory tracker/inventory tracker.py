dd77ec39545fe7c9e870cb78543dabf6030cecfa
Dafz-7 <orangksp@gmail.com>
2025-09-27 16:58:19 +0700

    added files

--- Python/projects/Inventory tracker/inventory tracker.py
+++ Python/projects/Inventory tracker/inventory tracker.py
@@ -1,5 +1,6 @@
 """
-- add items to inventory
+-DONE add items to inventory
+- remove items manually
 - remove items when out of stock
 - check current stock
 - alert item if low on stock (less than 2)
@@ -11,14 +12,31 @@
 """
 
 # functions
-inventory = {}
+inventory_dict = {}
+inventory_list = []
 
+def add_items():
+    while True:
+        items = input("One item to add: ").lower()
+        quantity = input("Amount in number(s): ")
+        if quantity.isdigit():
+            quantity = int(quantity)
+            if 0 < quantity < 10000:
+                break
+            else:
+                print("Amount must be > 0 and < 10000.")
+        else:
+            print("Please enter a valid number.")
+    inventory_tuple = (items, quantity)
+    inventory_list.append(inventory_tuple)
+    for items, quantity in inventory_list:
+        inventory_dict[items] = quantity
 
 # Main program
 print(
     """
     --- My inventory ---
-    Type the following keywords (ignore the hyphen):
+    Type the following keywords:
 
     - add item
     - remove item
@@ -26,4 +44,8 @@ print(
 
     """
 )
+
+add_items()
+print(inventory_list)
+print(inventory_dict)
 
